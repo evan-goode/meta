3d4232ee988cbfa750c12011421d2470d4fea24f
Evan Goode <mail@evangoo.de>
2024-07-02 20:28:18 -0400

    Rebase on PrismLauncher/meta

---
+++ config.sh
@@ -0,0 +1,11 @@
+export META_UPSTREAM_URL=git@github.com:unmojang/meta-upstream-test.git
+export META_LAUNCHER_URL=git@github.com:unmojang/meta-launcher-test.git
+
+export META_UPSTREAM_DIR=upstream
+export META_LAUNCHER_DIR=launcher
+
+export DEPLOY_TO_GIT=true
+export GIT_AUTHOR_NAME="github-actions"
+export GIT_AUTHOR_EMAIL="mail@unmojang.org"
+export GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"
+export GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
